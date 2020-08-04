50b7ca63896fb6f31f27774aaea54cabbd3e9cba
yarvinh <yarvinhhernandez@gmail.com>
2020-08-04 17:38:39 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,11 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+    set :database, 'sqlite3:db/database.db'
+  end
+
+  configure :development do
+    set :database, 'sqlite3:db/dogs.db'
+  end
 require './app'
